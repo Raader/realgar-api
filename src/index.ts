7c6aa5940389ea524a87ed0eeaf5e8120ababc31
Efe <48628713+Raader@users.noreply.github.com>
2021-08-02 15:18:40 +0300

    basic crud api

--- src/index.ts
+++ src/index.ts
@@ -21,5 +21,36 @@ app.get("/payments", async (req, res, next) => {
     next(error);
   }
 });
+
+app.get("/payments/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const payment = await paymentService.readById(id);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.patch("/payments/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const payment = await paymentService.updateById(id, req.body);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/payments/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const payment = await paymentService.deleteById(id);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("server started on on port: " + port));
