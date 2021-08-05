26991f5bcd18b32ffbb2fd2449bdf5136b25114c
Efe <48628713+Raader@users.noreply.github.com>
2021-08-05 18:14:40 +0300

    seperate express app

---
+++ src/api/index.ts
@@ -0,0 +1,75 @@
+import express from "express";
+import cors from "cors";
+import paymentService from "../payment";
+import githubService from "../oauth/github";
+
+//middlewares
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.post("/payments", async (req, res, next) => {
+  try {
+    const payment = await paymentService.create(req.body);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/payments", async (req, res, next) => {
+  try {
+    const payments = await paymentService.read();
+    res.json(payments);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/payments/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const payment = await paymentService.readById(id);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.patch("/payments/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const payment = await paymentService.updateById(id, req.body);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/payments/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const payment = await paymentService.deleteById(id);
+    res.json(payment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/oauth/github", async (req, res, next) => {
+  res.send(githubService.authorizeURL);
+});
+
+app.post("/oauth/github", async (req, res, next) => {
+  const { code } = req.query;
+  try {
+    if (typeof code !== "string") throw new Error("query params invalid");
+    const user = await githubService.authenticate(code);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,8 @@
+//configure environment variables
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
-import paymentService from "./payment";
 
-import githubService from "./oauth/github";
-
-const app = express();
-app.use(express.json());
-
-app.post("/payments", async (req, res, next) => {
-  try {
-    const payment = await paymentService.create(req.body);
-    res.json(payment);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get("/payments", async (req, res, next) => {
-  try {
-    const payments = await paymentService.read();
-    res.json(payments);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get("/payments/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const payment = await paymentService.readById(id);
-    res.json(payment);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.patch("/payments/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const payment = await paymentService.updateById(id, req.body);
-    res.json(payment);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.delete("/payments/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const payment = await paymentService.deleteById(id);
-    res.json(payment);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.post("/oauth/github", async (req, res, next) => {
-  const { code } = req.query;
-  try {
-    if (typeof code !== "string") throw new Error("query params invalid");
-    const user = await githubService.authenticate(code);
-    res.json(user);
-  } catch (error) {
-    next(error);
-  }
-});
-
+import app from "./api";
+//start the server
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("server started on on port: " + port));
+app.listen(port, () => console.log("app started at port: " + port));
