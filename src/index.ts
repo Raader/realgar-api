2d99b3760e4971f73c0a01c7ce26715f311c735a
Efe <48628713+Raader@users.noreply.github.com>
2021-08-22 14:23:33 +0300

    Add starting notifications

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./api";
+import { startNotificationInterval } from "./notification";
+
 //start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("app started at port: " + port));
+
+startNotificationInterval();
---
+++ src/notification/index.ts
@@ -0,0 +1,33 @@
+import emailService from "../email";
+import paymentService from "../payment";
+import userService from "../user";
+import PaymentNotificationService from "./notification_service";
+
+const delay = Number(process.env.NOTIFICATION_INTERVAL) || 300000;
+
+let interval: NodeJS.Timer;
+
+const notificationService = new PaymentNotificationService(
+  userService,
+  paymentService
+);
+
+function handleInterval() {
+  console.log("started sending notifications");
+  notificationService.sendNotifications((user, payment) => {
+    emailService.sendMail({
+      to: user.email,
+      from: "realgar@raader.me",
+      subject: `Payment notification for ${payment.name}`,
+      text: `You are about to be charged ${payment.price}${payment.currency} for ${payment.name}`,
+    });
+  });
+}
+
+export function startNotificationInterval(): void {
+  interval = setInterval(handleInterval, delay);
+}
+
+export function stopNotificationInterval(): void {
+  clearInterval(interval);
+}
